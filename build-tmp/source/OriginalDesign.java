44c5790e93ed1161bb7ef995b7aa4d97ce648abe
sammyrms1 <super.sammy2011@gmail.com>
2015-08-25 11:36:44 -0700

    Added N-gear

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,4 +1,20 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
+public void setup()
 {
 //size(400, 400); //Square res
 //size(854, 480); //widescreen
@@ -8,16 +24,16 @@ background(0, 0, 255);
 //String[] fontList = PFont.list();
 //println(fontList);
 }
-void draw()
+public void draw()
 {
 	//ball();
 	nGear();
 }
-void ball()
+public void ball()
 {
   ellipse(400,350,200,200);
 }
-void nGear()
+public void nGear()
 	{
 	PFont nGearFont;
 	nGearFont = createFont("SansSerif.plain", 30);
@@ -68,3 +84,12 @@ void nGear()
 
 
 	}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
